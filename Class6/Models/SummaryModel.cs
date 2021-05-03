61a2eb04411680856e4d1f101c14ba352c51e693
Asif Abdullah <asifabdullah135@gmail.com>
2021-05-03 14:17:35 +0600

    MVC

---
+++ Class6/Models/SummaryModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Class6.Models
+{
+    public class SummaryModel
+    {
+        public string SummaryText { get; set; }
+
+        public SummaryModel()
+        {
+            SummaryText = "Hello World";
+        }
+    }
+}
