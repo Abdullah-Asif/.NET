e285a828f6959ca6ee8e77f7b146edfeecc02c21
Asif Abdullah <asifabdullah135@gmail.com>
2021-06-01 09:59:48 +0600

    Delegate

---
+++ Class3-0/Class3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Class3
+{
+    class Program
+    {
+        delegate void Perform(string text);
+        static void Main(string[] args)
+        {
+            var logic = new Perform(PrintingStyle2);
+            var text = "Hello World";
+            ProcessText(text, logic);
+
+        }
+        public static void PrintingStyle1(string text)
+        {
+            Console.WriteLine($"*** {text} ***");
+        }
+        public static void PrintingStyle2(string text)
+        {
+            Console.WriteLine($"--- {text} ---");
+        }
+         static void ProcessText(string text, Perform  perform)
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                perform(text);
+            }
+        }
+    }
+}
