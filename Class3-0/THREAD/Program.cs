39df5a520ab842c182367c5d77c4abade2055b91
Asif Abdullah <asifabdullah135@gmail.com>
2021-06-01 15:11:03 +0600

    Thread

---
+++ Class3-0/THREAD/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace THREAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            var thread1 = new Thread(OddPrint);
+            var thread2 = new Thread(new ThreadStart(program.EvenPrint));
+            thread2.Start();
+             thread1.Start();
+        }
+        static void OddPrint()
+        {
+            for (int i = 1; i < 100; i += 2)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+        }
+         void EvenPrint()
+        {
+            for (int i = 0; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+
+            }
+        }
+    }
+}
